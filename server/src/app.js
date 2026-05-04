459da6386caf41c31001813bca48fbf441cf3538
sahils2004 <sahilsarawgi1239@gmail.com>
2026-05-04 16:33:22 +0530

    Single Dockerfile at root level for both frontend and backend

--- server/src/app.js
+++ server/src/app.js
@@ -22,9 +22,17 @@ app.use('/api/books', require('./routes/bookRoutes'));
 // Auth Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 
+const path = require('path');
+const distPath = path.join(__dirname, '../../client/dist');
+app.use(express.static(distPath));
+
 // Root Route (optional, just to show something)
 app.get('/', (req, res) => {
-  res.send('ShopSmart Backend Service');
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 module.exports = app;
